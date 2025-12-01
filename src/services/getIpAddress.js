dfd6995dbe145323935e5162bb32eb768895d0f3
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2025-11-30 21:39:52 -0500

    added comments on the API services module

--- src/services/getIpAddress.js
+++ src/services/getIpAddress.js
@@ -1,5 +1,8 @@
+// Funny API key insecurely stored here lol. It's free though
 const API_KEY = 'at_goKVzx944pBGnH68vsN8MT6A0wSbu';
 
+// Async function to get IP address data from the IPify API given an IP or domain
+// Returns a promise containing the fetched data
 const getIpAddress = async (ip = '', domain = '') => {
     const response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}&domain=${domain}`);
 
