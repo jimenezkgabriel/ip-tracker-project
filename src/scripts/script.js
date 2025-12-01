2b0acd750d8cc1c965ae61558804ba715e747623
jimenezkgabriel <jimenez.k.gabriel@gmail.com>
2025-11-30 22:31:21 -0500

    working on a bug of the map, also fixed a the arrow image for the button

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -11,7 +11,7 @@ const timezone = document.getElementById('timezone');
 const isp = document.getElementById('isp');
 
 // Initialize the map
-let map = L.map('map');
+let map = L.map('map').setView([0, 0], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
